af6f1650d19e5e3e44d23fd33f1cdd8d540cfd9c
G6B0 <146021873+G6B0@users.noreply.github.com>
2023-09-27 18:51:48 -0300

    Avances en clase DetalleOrden
    Se añadieron parametros al constructor y se calculan con exito los metodos.

--- DetalleOrden.java
+++ DetalleOrden.java
@@ -3,18 +3,20 @@ import java.util.ArrayList;
 public class DetalleOrden {
     private int cantidad;
     private Articulo articulo;
-    public DetalleOrden(){
+    public DetalleOrden(int cantidad,Articulo articulo){
+        this.cantidad=cantidad;
+        this.articulo=articulo;
     }
     public float calcPrecio(){
         return calcPrecioSinIva()+calcIva();
     }
     public float calcPrecioSinIva(){
-        return 0;
+        return articulo.getPrecio()*cantidad;
     }
     public float calcIva(){
         return calcPrecioSinIva()*0.19f;
     }
     public float calcPeso(){
-        return 0;
+        return articulo.getPeso()*cantidad;
     }
 }
