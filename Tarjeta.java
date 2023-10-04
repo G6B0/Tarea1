b48a82f6656d8e23cd326a66f21a72a64c389094
G6B0 <146021873+G6B0@users.noreply.github.com>
2023-10-04 13:56:40 -0300

    Creacion de metodo AgregarPlazo
    AgregarPlazo(pagonuevo,fecha) metodo para pagar en plazos.

--- Tarjeta.java
+++ Tarjeta.java
@@ -4,10 +4,16 @@ class Tarjeta extends Pago{
     private String Tipo;
     private int Plazos;
     private String numTransaccion;
+    private float montoapagar;
     public Tarjeta(String tipo, String numTransaccion, int plazos, LocalDate fecha, OrdenCompra orden){
         super(fecha,orden);
         this.Tipo=tipo;
         this.numTransaccion=numTransaccion;
-
+        this.Plazos=plazos;
+        montoapagar=getMonto()-Plazos;
+    }
+    public void AgregarPlazo(int pagonuevo,LocalDate fecha){
+        montoapagar= montoapagar-pagonuevo;
+        fecha = LocalDate.now();
     }
 }
--- Transferencia.java
+++ Transferencia.java
@@ -4,12 +4,23 @@ class Transferencia extends Pago {
 
     private String Banco;
     private String numCuenta;
-    private int plazos;
+    private LocalDate fecha;
+    private float cuant;
+    private float montoapagar;
 
     public Transferencia(String banco, String numcuenta,LocalDate fecha,OrdenCompra orden, float cuant) {
         super(fecha, orden);
         this.Banco = banco;
         this.numCuenta = numcuenta;
+        this.fecha=fecha;
+        this.cuant=cuant;
+        montoapagar= orden.calcPrecio()-cuant;
     }
-    //public void AgregarPlazo();
+    public void AgregarPlazo(int pagonuevo,LocalDate fecha){
+        montoapagar= montoapagar-pagonuevo;
+        fecha =LocalDate.now();
+    }
+    public float getMontoapagar(){
+        return montoapagar;
+    }
 }
